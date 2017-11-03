e96d5918aebf8265e2c87dd791d3064ccf0c6bba
ChasobaAlpacanist <chasobakun@gmail.com>
2017-11-04 01:03:19 +0900

    マウス座標を画面に反映。確認はUNKで代用。改良の余地あり。

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,2 +1,46 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;public class PlayerController : MonoBehaviour {    public StoneMover stone;    bool launch;	void Start () {
-        launch = true;	}		void Update () {        if (Input.GetButton("Vertical") && launch) {            launch = false;            stone.Launch();         }	}}
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public StoneMover stone;
+    bool launch;
+
+    //Mouse`s position
+    private Vector3 position;
+    private Vector3 worldPositionofMouse;
+
+    public float zPosi;
+
+    public GameObject UNK;
+
+	void Start () {
+        launch = true;
+	}
+	
+	void Update () {
+        if (Input.GetButton("Vertical") && launch) {
+            launch = false;
+            stone.Launch(); 
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            GetMousePosition();
+        }
+	}
+
+    //Make pointed ground smoother (now Instanciates UNK)
+    void GetMousePosition() {
+        position = Input.mousePosition;
+            
+        //adjust z position
+        position.z = zPosi;
+        worldPositionofMouse = Camera.main.ScreenToWorldPoint(position);
+
+        Instantiate(UNK,
+            worldPositionofMouse,
+            Quaternion.identity);
+           
+    }
+}
